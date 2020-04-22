2596f02f064265abb153355c1e8847ccd2640365
liulin <liulin>
2020-04-22 17:34:56 +0800

    冒泡排序

--- redis-demo/src/test/java/com/bitbybit/redis/demo/CommonTests.java
+++ redis-demo/src/test/java/com/bitbybit/redis/demo/CommonTests.java
@@ -19,7 +19,7 @@ public class CommonTests {
     public void getTime() {
         System.out.println(System.nanoTime());
         System.out.println(System.currentTimeMillis());
-        logger.error("test logger error {}","ccc", new Exception("12321321"));
+        logger.error("test logger error {}", "ccc", new Exception("12321321"));
     }
 
     @Test
@@ -31,6 +31,21 @@ public class CommonTests {
             logger.info(a);
         } else if (Objects.isNull(b)) {
             logger.info(b);
+        }
+    }
+
+    @Test
+    public void bubbleSort() {
+        int[] arr = new int[]{4, 3, 2, 1};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            logger.info("arr = {}", arr);
         }
     }
 }
